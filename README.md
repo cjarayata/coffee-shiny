cdef20cc0c4092057a0528ba36ab0f520931a891
cjarayata <charles.arayata@gmail.com>
2022-03-22 00:13:39 -0400

    Got french press to work. Fixed misspelling in James HoffmanN's name.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # coffee-shiny
-A Shiny app to help me with making coffee in the morning. I have a lot of Post-Its with brew charts, pages in a notebook that tell me what ratio tastes best for which coffee, etc. A lot of this is based on "The Ultimate V60 Technique" by James Hoffman, but tweaked by me based on my experience thus far.
+A Shiny app to help me with making coffee in the morning. I have a lot of Post-Its with brew charts, pages in a notebook that tell me what ratio tastes best for which coffee, etc. A lot of this is based on James Hoffmann's YouTube videos, but tweaked by me based on my experience thus far.
 
-Watch James Hoffman's video here: https://www.youtube.com/watch?v=AI4ynXzkSQo
+Watch James Hoffmann's "Ultimate V60 Technique" video here: https://www.youtube.com/watch?v=AI4ynXzkSQo<br>
+Watch James Hoffmann's "Ultimate French Press Technique" video here: https://www.youtube.com/watch?v=st571DYYTR8<br>
 Check out the shiny app here: https://cjarayata.shinyapps.io/coffee-shiny/
--- coffee_functions.R
+++ coffee_functions.R
@@ -2,16 +2,16 @@ coffee_data <- read_csv("coffee_data.csv", show_col_types = F)
 
 # Grab the appropriate ratio for the method given. If the coffee doesn't exist,
 # return sensible defaults.
-find_ratio <- function(coffee_type, brew_method, coffee_lookup){
+find_ratio <- function(coffee, brew_method, coffee_lookup){
         
         coffee_row <- coffee_lookup %>% 
-                filter(coffee_brand == coffee_type)
+                filter(coffee_brand == coffee)
         
         # if coffee cannot be found, use 0.055 to start
-        if(nrow(coffee_row) == 0 && brew_method == "hoffman"){
+        if(nrow(coffee_row) == 0 && brew_method == "hoffmann v60"){
                 ratio <- 0.055
         }
-        if(nrow(coffee_row) == 0 && brew_method == "french press"){
+        if(nrow(coffee_row) == 0 && brew_method == "hoffmann french press"){
                 ratio <- 0.07
         }
         
@@ -19,10 +19,10 @@ find_ratio <- function(coffee_type, brew_method, coffee_lookup){
                 ratio <- 0.065
         }
         
-        if(nrow(coffee_row) >= 1 && brew_method == "hoffman"){
+        if(nrow(coffee_row) >= 1 && brew_method == "hoffmann v60"){
                 ratio <- coffee_row %>% pull(pourover_ratio)
         }
-        if(nrow(coffee_row) >= 1 && brew_method == "french press"){
+        if(nrow(coffee_row) >= 1 && brew_method == "hoffmann french press"){
                 ratio <- coffee_row %>% pull(french_press_ratio)
         }
         if(nrow(coffee_row) >= 1 && brew_method == "onyx"){
@@ -33,16 +33,17 @@ find_ratio <- function(coffee_type, brew_method, coffee_lookup){
 }
 
 find_grind_size <- function(brew_method){
-        grind_size <- case_when(brew_method == "hoffman" ~ 14,
-                                brew_method == "french press" ~ 32,
+        grind_size <- case_when(brew_method == "hoffmann v60" ~ 14,
+                                brew_method == "hoffmann french press" ~ 30,
                                 brew_method == "onyx" ~ 18)
         return(grind_size)
 }
 
 # Generate a data.frame that has pour phases and approximate times.
 # This will be the chart on which we'll bind the amounts calculated.
-pour_timing <- function(target_volume){
+pour_timing <- function(target_volume, brew_method){
         
+        if(brew_method == "hoffmann v60"){
         # Determine the target brew time based on final volume.
         # Larger quantities will have their pour phases slightly delayed to allow for draining.
         target_brew_time <- case_when(target_volume >= 801 ~ "~ 4:30+",
@@ -53,7 +54,7 @@ pour_timing <- function(target_volume){
                        target_volume >= 250 ~ "~ 3:00",
                        TRUE ~ "< 3:00")
         
-        # This is currently only written for Hoffman, but can be expanded for
+        # This is currently only written for Hoffmann v60, but can be expanded for
         # Onyx and French Press methods
         df <- data.frame(brew_phase = c("Bloom",
                                         "1st Pour",
@@ -73,12 +74,25 @@ pour_timing <- function(target_volume){
                                   "1:15 - 1:45",
                                   target_brew_time))
         return(df)
-}
+        }
+        }
+        # if brew method is french press, volume becomes irrelevant
+        if(brew_method == "hoffmann french press"){
+        df <- data.frame(brew_phase = c("Initial Pour",
+                                        "Stir & Scoop Crust",
+                                        "Waiting Time",
+                                        "Total Brew Time"))
+        df <- df %>% mutate(time = c("4 minutes",
+                                  "At 4 minutes",
+                                  "5 to 10 minutes",
+                                  "Up to 15 minutes"))
+        return(df)
+        }
 }
 
 # Function to output a nice gt() brew guide for making the defined coffee, volume, and brew method.
-# Currently only written for James Hoffman v60.
-give_me_coffee <- function(coffee, target_volume, brew_method = "hoffman", coffee_lookup){
+# Currently only written for James Hoffmann v60.
+give_me_coffee <- function(coffee, target_volume, brew_method = "hoffmann v60", coffee_lookup){
         
         # first, find ideal ratio for given brew method using function
         ratio <- coffee %>% find_ratio(brew_method, coffee_lookup)
@@ -90,19 +104,30 @@ give_me_coffee <- function(coffee, target_volume, brew_method = "hoffman", coffe
         coffee_needed <- round(ratio * target_volume, 0)
         bloom_amt <- coffee_needed * 2
         
+        if(brew_method == "hoffmann v60"){
         # use 60/40 split to get intermediate volumes for pour phases
-        pour_one_vol <- target_volume * .6
-        pour_two_vol <- target_volume * .4
+                pour_one_vol <- target_volume * .6
+                pour_two_vol <- target_volume * .4
+                
+                # use volume to determine what target brew time should be
+                new_df <- pour_timing(target_volume, brew_method) %>% 
+                        mutate(volume = c(bloom_amt,
+                                                pour_one_vol,
+                                                pour_two_vol,
+                                                target_volume))
+                
+        }
+        if(brew_method == "hoffmann french press"){
+        new_df <- pour_timing(target_volume, brew_method) %>% 
+                mutate(volume = target_volume)
         
-        # use volume to determine what target brew time should be
-        new_df <- pour_timing(target_volume) %>% 
-                mutate(volume = c(bloom_amt,
-                                        pour_one_vol,
-                                        pour_two_vol,
-                                        target_volume))
+        }
+        # return(new_df)
         
         # return a gt() table that has this information for you!
-        new_df %>% gt() %>% 
+        gt_table <- 
+                new_df %>% 
+                gt() %>% 
                 tab_header(title = md(glue("**Brew Guide for {str_to_title(brew_method)}**")),
                            subtitle = md(glue("**{coffee_needed}g** of {coffee}, Grind Size #{grind_size}"))) %>% 
                 cols_label(brew_phase = "Brew Phase",
@@ -111,5 +136,12 @@ give_me_coffee <- function(coffee, target_volume, brew_method = "hoffman", coffe
                 cols_align(align = c("center"),
                            columns = c(time, volume)) %>% 
                 opt_row_striping(TRUE) %>% 
+                tab_source_note(HTML("Grind sizes are for Baratza Encore. Refer to <a href = 'https://honestcoffeeguide.com/guides/coffee-grind-size-chart' target = '_blank'> Honest Coffee's Grind Size Tool</a> for guidance for your particular grinder."))
+        
+        if(brew_method == "hoffmann v60"){
+        gt_table <- gt_table %>%
                 tab_source_note("Note: Swirl the V60 during bloom phase and after second pour for even extraction.")
+        }
+        
+        return(gt_table)
 }
